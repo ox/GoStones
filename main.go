46a609be084f09f28d399b5af4f889ee4588fa89
Artem Titoulenko <owlhuntr92@gmail.com>
2009-11-29 19:13:33 -0500

    vectors are GemSource instead of just strings. Removal of repo is fixed and working

--- main.go
+++ main.go
@@ -57,22 +57,33 @@ var (
 )
 
 var (
+	errors vector.Vector;
 	repositories vector.StringVector; 
 	ik int; 
 )
 
 type GemSource struct {
-	short_name string;
+	short_names []string;
 	url string;
 	}
+	
+	func NewGemSource(short_names []string, url string) *GemSource {
+		return &GemSource{short_names, url}
+	}
+	
+	func (g *GemSource) String() string {
+		return g.url
+	}
+	
+
 
 	
 
 func main() {
-	errors := vector.New(100);
+	errors := vector.New(0);
 	
 	//fill repositories StringVector
-	repositories := vector.NewStringVector(0); //make a new vec
+	repositories := vector.New(0); //make a new vec
 	file, err := os.Open("./list.txt", os.O_RDONLY, 0755); 
 	if file == nil { errors.Push(err); }
 	
@@ -82,7 +93,10 @@ func main() {
 	for {
 		dat2, err := in.ReadSlice('\n');
 		if err != nil || string(dat2[0:len(dat2)-1]) == "" { errors.Push(err); break; }
-		repositories.Push( string(dat2[0:len(dat2)-1]) );
+		
+		str := strings.Split(string(dat2), " ", -1);
+		gem := NewGemSource(str[1:len(str)], str[0]);
+		repositories.Push( gem );
 		
 		ik++;
 		}
@@ -91,7 +105,10 @@ func main() {
 	var get_go_gems_version *bool = flag.Bool("version", false, "Show the version number");
 	var show_errors *bool = flag.Bool("show_errors", false, "Show errors");
 	var list *bool = flag.Bool("list", false, "List all available repositories");
-	var add_repo *string = flag.String("add", "", "Add a repo to the list of known repositories");
+	var list_full *bool = flag.Bool("list-all", false, "list repositories with their associated aliases");
+	var add_repo *string = flag.String("add", "", "Add a repo to the list of known repositories <-add=\"url alias1 alias2\">");
+	var remove_repository *string = flag.String("remove-repository", "", "Remove selected repository and it's associated aliases by url or alias");
+	var remove_alias *string = flag.String("remove-alias", "", "Remove alias from a repository");
 	var install *string = flag.String("install", "", "Install GoGem");
 	flag.Parse();
 	
@@ -109,24 +126,114 @@ func main() {
 	if *list {
 		fmt.Println("listing",ik,"repos:");
 		for i := 0; i < ik; i++ {
-			fmt.Println( repositories.At(i) );
+			fmt.Printf("%v\n", repositories.At(i).(*GemSource).url );
 			}
 			br();
 	}
 	
+	if *list_full {
+		for i := 0; i < ik; i++ {
+			fmt.Printf("%v\n----------------------\n    ", repositories.At(i).(*GemSource) );
+			for _, v := range repositories.At(i).(*GemSource).short_names {
+				fmt.Printf("%v\n    ",v);
+			}
+			br();
+		}
+	}
+	
+
+	
+	if *remove_repository != "" {
+		
+		fmt.Println("looking for", *remove_repository);
+		
+		l := 0;
+		
+		for ; l < ik; l++ {
+			gem := repositories.At(l).(*GemSource);
+			
+			if gem.url == *remove_repository {
+				fmt.Print("found! removing ", gem.url, " at ", l,"\n");
+				repositories.Delete(l);
+				break;
+			}
+			
+			for _, v := range gem.short_names {
+				if v == *remove_repository || v == *remove_repository + "\n" {
+					fmt.Print("found! removing ", v, " at ", l,"\n");
+					repositories.Delete(l);
+					l = ik;
+					break;
+				}
+			}
+		}
+				
+		if repositories.Len() == ik { //nothing was removed
+			//our fall through
+			fmt.Println("No such alias or url found in the list, check spelling or url,", repositories.Len());
+			os.Exit(1);
+		}
+				
+		file, err := os.Open("./list.txt", os.O_RDWR, 0755); 
+		if file == nil { errors.Push(err); }
+		
+		out = bufio.NewWriter( file );
+				
+		errlol := io.WriteFile(file.Name(), []byte{}, 0755);
+		if errlol != nil { 
+			fmt.Print(errlol); 
+			errors.Push(errlol); 
+			os.Exit(1); 
+		}
+		
+		for i := 0; i < repositories.Len(); i++ {
+			gem := repositories.At(i).(*GemSource);
+			io.WriteString(out, gem.url);
+			fmt.Print(gem);
+			
+			for k := 0; k < len(gem.short_names); k++ {
+				io.WriteString(out, " " + gem.short_names[k]);
+				fmt.Print(" " + gem.short_names[k]);
+			}
+			//io.WriteString(out, "\n" );
+		}
+				
+		out.Flush();	
+		file.Close();
+	}
+	
 	if *add_repo != "" {
-		repositories.Push(*add_repo);
+		str := strings.Split(*add_repo, " ", -1);
+		
+		var gem *GemSource;
+		
+		if len(str) == 1 { 
+			_, short_name := path.Split(str[0]);
+			short_name = strings.Split(short_name, ".",-1)[0];
+			gem = NewGemSource([]string{short_name + "\n"}, str[0]);
+			fmt.Println("no alias provided, making it", short_name);
+		} else {
+			gem = NewGemSource(str[1:len(str)], str[0]);
+		}
+		
+		fmt.Println("adding:", gem);
+		
+		repositories.Push(gem);
 		
 		file, err := os.Open("./list.txt", os.O_RDWR, 0755); 
 		if file == nil { errors.Push(err); }
 		
 		out = bufio.NewWriter( file );
 		
-		for i := 0; i < ik+1; i++ {
-			io.WriteString(out, repositories.At(i)+"\n" );
+		for i := 0; i < repositories.Len(); i++ {
+			io.WriteString(out, repositories.At(i).(*GemSource).url);
+			for k := 0; k < len(repositories.At(i).(*GemSource).short_names); k++ {
+				io.WriteString(out, " " + repositories.At(i).(*GemSource).short_names[k]);
+			}
+			//io.WriteString(out, "\n" );
 		}
 		out.Flush();	
-		file.Close();	
+		file.Close();
 	}
 	
 	if *install != "" {
@@ -134,15 +241,15 @@ func main() {
 		
 		//search the repos for the right file
 		for i := 0; i < ik; i++ {
-			_, pre_tested_name := path.Split( repositories.At(i) );
+			_, pre_tested_name := path.Split( repositories.At(i).(GemSource).short_names[0] );
 			tested_name := strings.Split( pre_tested_name, ".", -1)[0];
 			
 			if tested_name == file_name {
-				response, _, err := http.Get( repositories.At(i) );
+				response, _, err := http.Get( repositories.At(i).(GemSource).url );
 				if err != nil { fmt.Println( err ); }
 				
 				var nr int;
-				const buf_size = 0x1000;
+				const buf_size = 0x10;
 				buf := make ([]byte, buf_size);
 				nr, _ = response.Body.Read (buf);
 
@@ -181,4 +288,7 @@ func main() {
 	if *show_errors {
 		fmt.Println(errors);
 	}
+	
+	file.Close();
+	
 }
